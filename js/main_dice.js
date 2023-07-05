458d2ff9a4f04394f0d3c41bcc4f765e2983de30
acolombo09 <acolombo0911@gmail.com>
2023-07-05 14:48:05 +0200

    hrefs_&_notes_updated

--- js/main_dice.js
+++ js/main_dice.js
@@ -1,3 +1,22 @@
 /*
+Gioco dei dadi:
 
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+---------------------------------------------------------------------------------
+BONUS: far inserire 3 numeri all’utente, generare per il pc 3 numeri, e dopo 
+aver calcolato la somma dei numeri dell’utente e dei numeri del pc, 
+capire chi ha vinto in base alla somma maggiore.
+---------------------------------------------------------------------------------
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che “cosa” ci serve?
+Consigli del giorno:
+1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+3. si ma noi cosa vogliamo fare?
+4. torniamo a scrivere in italiano
+5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma 
+così come lo faremmo “a mano”
+Buon lavoro e buon divertimento!
 */
